a9ffec842afba47b8c81bb3386c24994874786c5
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-12-03 00:04:55 +0000

    ✨ feat: create Input component

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,35 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {label && (
+        <FormLabel id={`${name}-form-label`} htmlFor={name}>
+          {label}
+        </FormLabel>
+      )}
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bg: 'gray.900',
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
